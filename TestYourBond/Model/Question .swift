4e0aab2666e739b0dbe716405827c6db4f3df2f2
Igor Palyvoda <tib.igor.mk@gmail.com>
2019-06-04 21:25:07 +0300

    add 2 file

---
+++ TestYourBond/Model/Question .swift
@@ -0,0 +1,9 @@
+//
+//  Question .swift
+//  TestYourBond
+//
+//  Created by Ingvar on 6/4/19.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import Foundation
---
+++ TestYourBond/Model/QuestionBank.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionBank.swift
+//  TestYourBond
+//
+//  Created by Ingvar on 6/4/19.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import Foundation
